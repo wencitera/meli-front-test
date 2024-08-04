feb15cc3ca315839ecb8e969619fdd13c7cbf644
Wenceslao Citera <wencitera@gmail.com>
2024-08-04 00:47:54 -0300

    Startup for routes

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Home from '../pages/Home';
+import { Route, Routes } from 'react-router-dom';
 
 const App = (props) => {
 
     return (
         <>
+            <Routes>
+                <Route exact path="/" element={<Home/>} />
+            </Routes>
         </>
     );
 };
