0a3caff193d6d35d33001f551882b0071e44c716
Wenceslao Citera <wencitera@gmail.com>
2024-08-10 19:47:32 -0300

    Lazy loading

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import Home from '../pages/Home';
+import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import SearchNav from './SearchNav/SearchNav';
-import ItemList from '../pages/ItemList/ItemList';
-import ProductDetail from '../pages/ProductDetail/ProductDetail';
+import { lazy } from 'react';
+import Loader from './Loader/Loader';
 
-const App = (props) => {
+const SearchNav = lazy(() => import('./SearchNav/SearchNav'));
+const Home = lazy(() =>  import('../pages/Home'));
+const ItemList = lazy(() => import('../pages/ItemList/ItemList'));
+const ProductDetail = lazy(() => import('../pages/ProductDetail/ProductDetail'));
+
+const App = () => {
 
     return (
-        <>
+        <Suspense fallback={<Loader />}>
             <SearchNav />
             <Routes>
                 <Route path="/" element={<Home />}/>
                 <Route path='/items' element={<ItemList/>} />
                 <Route path='/item/:id' element={<ProductDetail />} />
             </Routes>
-        </>
+        </Suspense>
     );
 };
 
